0f5f82f1360e76e6bf23a0aa9d0afed031e6b235
Andressa Santos <100451911+AndressaMSantos@users.noreply.github.com>
2022-12-08 16:06:25 -0300

    Update procAltLivro.php

--- aula_09-23-22/adm/procAltLivro.php
+++ aula_09-23-22/adm/procAltLivro.php
@@ -1,2 +1,19 @@
 <?php
 session_start();
+include "../servidor.php";
+// Variaveis
+$titulo = $_POST["titulo"];
+$desc = $_POST["desc"];
+$valor = $_POST["valor"];
+$ed = $_POST["ed"];
+$id = $_POST["cod_liv"];
+
+
+
+$sql = "UPDATE tb_livro SET titulo_liv = '$titulo', desc_liv = '$desc', valor_liv = '$valor', cod_ed = '$ed' WHERE cod_liv = '$id'";
+$resp = $OOP->query($sql);
+
+if ($OOP->affected_rows) {
+    echo "<script type='text/javascript'>
+    alert('Alteração feita!');
+    </script>";
