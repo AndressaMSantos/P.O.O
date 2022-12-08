9bdcba79b9270dbe6fe349f849500fd77953eb71
Andressa Santos <100451911+AndressaMSantos@users.noreply.github.com>
2022-12-08 16:00:35 -0300

    Update altLivro.php

--- aula_09-23-22/adm/altLivro.php
+++ aula_09-23-22/adm/altLivro.php
@@ -1,8 +1,8 @@
 <?php
-// iniciar session;
+
 session_start();
-// incluir o servidor
-include_once("../servidor.php");
+
+include_once "../servidor.php";
 
 ?>
 
@@ -16,82 +16,104 @@ include_once("../servidor.php");
     <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
     <title>Document</title>
 </head>
+
 <body>
     <div class="container">
         <div class="row">
             <section class="col-md-2">
+
             </section>
-                <?php
-                    //PEGAR ID
-                    $id = $_GET["cod_liv"];
-                    //QUERY
-                    $sql = " SELECT * FROM TB_LIVRO WHERE cod_liv = ". $id;
 
-                    //EXECUTAR
-                    $resp = mysqli_query($banco, $sql);
+            <?php
 
-                    // COLOCAR NO CAMPO
+//pegar id
+$id = $_GET["cod_liv"];
+//query
+$sql = " SELECT * FROM tb_livro WHERE cod_liv =" . $id;
+//executar
+//$resposta = mysqli_query($banco, $sql);
 
-                    $campo = mysqli_fetch_array($resp);
-                ?>
+$resposta = $OOP->query($sql);
+//colocar os campos
+//$campo = mysqli_fetch_array($resposta);
+
+$campo = $resposta->fetch_array();
+?>
+
             <section class="col-md-8">
                 <h3 class="mt-5">Altera Livro</h2>
+
                     <form action="procAltLivro.php" method="post" enctype="multipart/form-data">
-                    <input type="hidden" name="cod_liv" value="<?php echo $campo["cod_liv"] ?>">                 
-                    <div class="form-group">
+                        <input type="hidden" name="cod_liv" value="<?php echo $campo["cod_liv"] ?>">
+
+
+                        <div class="form-group">
                             <label for="t">Titulo : </label>
-                            <input type="text" class="form-control" id="t" name="titulo" 
-                            value="<?php echo $campo["titulo_liv"] ?>">
+                            <input type="text" class="form-control" id="t" name="titulo"
+                                value="<?php echo $campo["titulo_liv"] ?>">
                         </div>
                         <div class="form-group">
                             <label for="desc">Descrição : </label>
-                            <textarea name="desc" class="form-control" id="desc"><?php echo $campo["desc_liv"] ?></textarea>
-                        </div> 
+                            <textarea name="desc" class="form-control"
+                                id="desc"><?php echo $campo["desc_liv"] ?></textarea>
+                        </div>
+
+
+
                         <div class="form-group">
                             <label for="ed">Editora: </label>
-                            <select class="form-control" name="ed" id="ed">
+
                             <?php
-                                // fazer a query no banco
-                                $editora = "SELECT * FROM tb_editora";
-                                //executar
-                                $resultado = mysqli_query($banco, $editora);
-                            ?>
+//query
+$sqlEditora = "SELECT * FROM tb_editora ";
+
+//executar
+$editora = $OOP->query($sqlEditora);
+?>
+
+
+
+                            <select class="form-control" name="ed" id="ed">
+
                                 <option>Selecione ...</option>
                                 <?php
-                                    //array                                    
-                                    while ($tbEditora = mysqli_fetch_array($resultado)){
-                                        echo 
-                                            "
-                                                <option value='".$tbEditora["cod_ed"]."'";
-                                                    if($campo["cod_ed"] == $tbEditora["cod_ed"])
-                                                    {                                                        
-                                                        echo "selected";
-                                                    } 
-                                                echo ">"
-                                                    .$tbEditora["nome_ed"]."
-                                                </option>
-                                            ";
-                                    }
-                                ?>
+while ($tbEditora = $editora->fetch_array()) {
+    echo "
+    <option value='" . $tbEditora["cod_ed"] . "' ";
+    if($tbEditora["cod_ed"] == $campo["cod_ed"]){
+        echo "selected";
+    }
+    echo " >"
+    . $tbEditora["nome_ed"] . "
+    </option>
+    ";
+}
+?>
+
                             </select>
+
                         </div>
+
+
                         <div class="form-group " style="width:30%;">
                             <label for="valor">Valor: </label>
+
                             <div class="input-group">
                                 <div class="input-group-prepend">
                                     <div class="input-group-text">$</div>
                                 </div>
                                 <input type="text" class="form-control" id="valor" name="valor"
-                                value="<?php echo $campo["valor_liv"] ?>"
-                                >
+                                    value="<?php echo $campo["valor_liv"] ?>">
                             </div>
                         </div>
                         <button type="submit" class="btn btn-primary">Alterar</button>
                     </form>
             </section>
+
             <section class="col-md-2"></section>
         </div>
     </div>
+
 </body>
 <script src="../js/jquery-3.5.1.slim.min.js"></script>
 <script src="../js/popper.min.js"></script>
