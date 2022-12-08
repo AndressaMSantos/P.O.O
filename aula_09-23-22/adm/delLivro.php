70bd5dbbc8d01e11d1a107e49226bbebc54c2643
Andressa Santos <100451911+AndressaMSantos@users.noreply.github.com>
2022-12-08 16:12:43 -0300

    Update delLivro.php

--- aula_09-23-22/adm/delLivro.php
+++ aula_09-23-22/adm/delLivro.php
@@ -1,43 +1,43 @@
-<?php
-// iniciar session;
-session_start();
-// incluir o servidor
-include("../servidor.php");
+<?php 
+    session_start();
+    include_once "../servidor.php";
 ?>
+
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Deletar</title>
+	
+	
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
 </head>
+
 <body>
     <div class="container">
-    <?php
-        //PEGA ID
-        $id = $_GET["cod_liv"];
 
-        // QUERY
+<?php
 
-        $sql = " DELETE FROM TB_LIVRO WHERE cod_liv = ". $id;
 
-        //EXECUTAR
+$id = $_GET["cod_liv"];
+$sql = " DELETE FROM tb_livro WHERE cod_liv = " . $id;
+$OOP->query($sql);
 
-        mysqli_query($banco, $sql);
 
-        if(mysqli_affected_rows($banco) == 1){
-            echo 
-                "
-                    <script>
-                        alert('Deletado com Sucesso !!!');
-                        location.href='lista_livro.php';                  
-                    </script>
-                ";
-        }
-    ?>
+if ($OOP->affected_rows) {
+
+    echo    "<script>
+                alert('Deletado com sucesso!');
+                window.location.href='lista_livro.php'
+            </script>";
+}
+?>
 </body>
+
 <script src="../js/jquery-3.5.1.slim.min.js"></script>
 <script src="../js/popper.min.js"></script>
+
 </html>
